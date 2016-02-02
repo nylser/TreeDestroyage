b7990c80486219de1b1a4f90dd615fef461381e6
Korbinian Stein <korbinian.stein@googlemail.com>
2016-02-02 15:20:24 +0100

    Remove LogSpam, new algorithms, first destroy blocks that are closer to the player.

--- src/main/java/net/mineguild/TreeDestroyage/event/BreakBlockHandler.java
+++ src/main/java/net/mineguild/TreeDestroyage/event/BreakBlockHandler.java
@@ -1,6 +1,8 @@
 package net.mineguild.TreeDestroyage.event;
 
 
+import com.flowpowered.math.vector.Vector3d;
+import com.flowpowered.math.vector.Vector3i;
 import com.google.common.collect.Lists;
 import com.google.common.reflect.TypeToken;
 import net.mineguild.TreeDestroyage.TreeDestroyage;
@@ -13,6 +15,7 @@ import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.Transaction;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.data.type.TreeTypes;
+import org.spongepowered.api.effect.sound.SoundTypes;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.Item;
@@ -30,6 +33,7 @@ import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -62,10 +66,21 @@ public class BreakBlockHandler {
                     }
                 }
                 TreeDetector dec = new TreeDetector(breakEvent.getTransactions().get(0).getOriginal(), maxAmount, getConfig());
+                Vector3d playerPos = cause.getLocation().getPosition();
                 List<Transaction<BlockSnapshot>> transactions = new ArrayList<>(dec.getWoodLocations().size());
-                dec.getWoodLocations().stream().filter(snapshot -> !snapshot.equals(breakEvent.getTransactions().get(0).getOriginal())).forEach(blockSnapshot -> {
+                dec.getWoodLocations().stream().sorted((snap1, snap2) -> {
+                    double distance1 = snap1.getPosition().toDouble().distance(playerPos);
+                    double distance2 = snap2.getPosition().toDouble().distance(playerPos);
+                    if (distance1 < distance2) {
+                        return -1;
+                    } else if (distance1 > distance2) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }).filter(snapshot -> !snapshot.equals(breakEvent.getTransactions().get(0).getOriginal())).forEach(blockSnapshot -> {
                     BlockState newState = BlockTypes.AIR.getDefaultState();
-                    BlockSnapshot newSnapshot = blockSnapshot.withState(newState).withLocation(new Location<World>(cause.getWorld(), blockSnapshot.getPosition()));
+                    BlockSnapshot newSnapshot = blockSnapshot.withState(newState).withLocation(new Location<>(cause.getWorld(), blockSnapshot.getPosition()));
                     Transaction<BlockSnapshot> t = new Transaction<>(blockSnapshot, newSnapshot);
                     transactions.add(t);
                 });
@@ -77,16 +92,17 @@ public class BreakBlockHandler {
                         if (cause.getGameModeData().get(Keys.GAME_MODE).get() != GameModes.CREATIVE) {
                             BlockState state = blockSnapshotTransaction.getOriginal().getState();
                             ItemStack.Builder builder = getGame().getRegistry().createBuilder(ItemStack.Builder.class);
-                            ItemStack itemStack = builder.fromBlockState(state.getType().getDefaultState()).build();
+                            ItemStack itemStack = builder.itemType(state.getType().getDefaultState().getType().getItem().get()).build();
+                            itemStack.offer(Keys.TREE_TYPE, state.get(Keys.TREE_TYPE).get());
                             Entity entity = cause.getWorld().createEntity(EntityTypes.ITEM, blockSnapshotTransaction.getOriginal().getPosition()).get(); // 'cause' is the player
                             entity.offer(Keys.REPRESENTED_ITEM, itemStack.createSnapshot());
                             cause.getWorld().spawnEntity(entity, Cause.of(cause));
                             if (item.supports(Keys.ITEM_DURABILITY)) {
-                                if(item.get(Keys.ITEM_DURABILITY).get() == 0){
+                                if (item.get(Keys.ITEM_DURABILITY).get() == 0) {
+                                    cause.getWorld().playSound(SoundTypes.ITEM_BREAK, cause.getLocation().getPosition(), 1);
                                     cause.setItemInHand(null);
                                 } else {
-                                    plugin.getLogger().info(item.offer(Keys.ITEM_DURABILITY, item.get(Keys.ITEM_DURABILITY).get() - 1).toString());
-
+                                    item.offer(Keys.ITEM_DURABILITY, item.get(Keys.ITEM_DURABILITY).get() - 1);
                                     cause.setItemInHand(item);
                                 }
                             }
