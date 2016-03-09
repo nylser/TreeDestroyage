86d962ba9b63c87c19ebe7114ff02bb82ba40c5d
Korbinian Stein <korbinian.stein@googlemail.com>
2016-03-09 18:21:42 +0100

    Fix packages.

---
+++ src/main/java/net/mineguild/minecraft/treedestroyage/TreeDestroyage.java
@@ -0,0 +1,214 @@
+package net.mineguild.minecraft.treedestroyage;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.reflect.TypeToken;
+import com.google.inject.Inject;
+import net.minecrell.mcstats.SpongeStatsLite;
+import net.mineguild.minecraft.treedestroyage.commands.SetConfigCommand;
+import net.mineguild.minecraft.treedestroyage.event.BreakBlockHandler;
+import net.mineguild.minecraft.treedestroyage.event.SaplingProtectionHandler;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStartingServerEvent;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.text.Text;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import static org.spongepowered.api.command.args.GenericArguments.*;
+
+@Plugin(id = "net.mineguild.minecraft.treedestroyage", description = "A plugin that allows to log trees quickly!", name = "TreeDestroyage", version = "0.8-DEV-API4.0.3")
+public class TreeDestroyage {
+
+    @Inject
+    private PluginContainer container;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private File defaultConfig;
+
+    @Inject
+    public SpongeStatsLite stats;
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private Game game;
+    private CommentedConfigurationNode config;
+
+    private BreakBlockHandler breakBlockHandler;
+    private SaplingProtectionHandler saplingHandler;
+
+    public Game getGame() {
+        return game;
+    }
+
+    @Listener
+    public void onPreInit(GamePreInitializationEvent event) {
+        this.stats.start();
+    }
+
+    @Listener
+    public void onInitialization(GameInitializationEvent event) {
+        breakBlockHandler = new BreakBlockHandler(this);
+        Sponge.getGame().getEventManager().registerListeners(this, breakBlockHandler);
+        saplingHandler = new SaplingProtectionHandler(this);
+        Sponge.getGame().getEventManager().registerListeners(this, saplingHandler);
+    }
+
+    @Listener
+    public void onServerStart(GameStartingServerEvent event) {
+        config = null;
+        try {
+            if (!defaultConfig.exists()) {
+                if (!defaultConfig.createNewFile()) {
+                    throw new RuntimeException("Unable to create configuration file!");
+                }
+                config = configManager.load();
+                config.getNode("version").setValue(1);
+                configManager.save(config);
+            }
+            config = configManager.load();
+
+
+            //Migrations here
+            boolean outOfDate;
+            do {
+                outOfDate = configMigration();
+            } while (outOfDate);
+
+            //Validation checks here
+            List<String> items = config.getNode("items").getList(TypeToken.of(String.class));
+            List<String> newItems = Lists.newArrayList(items);
+            List<String> toRemove = new ArrayList<>();
+            items.stream().filter(id -> !game.getRegistry().getType(ItemType.class, id).isPresent()).forEach(toRemove::add);
+            toRemove.forEach(newItems::remove);
+            config.getNode("items").setValue(newItems);
+            configManager.save(config);
+
+        } catch (IOException e) {
+            getLogger().error("Unable to load/create config!", e);
+        } catch (ObjectMappingException e) {
+            e.printStackTrace();
+        }
+        Set<String> newSet = Sets.newHashSet();
+        config.getChildrenMap().keySet().forEach(str -> newSet.add((String) str));
+        Map<String, String> choices = new HashMap<>();
+        for (Object obj : config.getChildrenMap().keySet()) {
+            choices.put((String) obj, (String) obj);
+        }
+
+
+        CommandSpec setSpec = CommandSpec.builder().arguments(onlyOne(choices(Text.of("setting"), choices)), optional(firstParsing(bool(Text.of("value")), integer(Text.of("value")), catalogedElement(Text.of("value"), ItemType.class)))).description(Text.of("Change config values on-the-fly")).executor(new SetConfigCommand(this))
+                .permission("TreeDestroyage.set").build();
+
+        CommandSpec reloadSpec = CommandSpec.builder().executor((src, args) -> {
+                    try {
+                        config = configManager.load();
+                        src.sendMessage(Text.of("Config reloaded!"));
+                        return CommandResult.success();
+                    } catch (IOException e) {
+                        src.sendMessage(Text.of("Config couldn't be reloaded!"));
+                        getLogger().error("Couldn't re-load config", e);
+                        return CommandResult.empty();
+                    }
+                }
+        ).permission("TreeDestroyage.reload").build();
+        CommandSpec mainSpec = CommandSpec.builder().child(setSpec, "set").arguments(none()).build();
+        Sponge.getCommandManager().register(this, setSpec, "trds");
+        saplingHandler.activate();
+    }
+
+    @Listener
+    public void onServerStop(GameStoppingServerEvent event) {
+        try {
+            configManager.save(config);
+        } catch (IOException e) {
+            getLogger().error("Unable to save config!", e);
+        }
+    }
+
+    public boolean configMigration(){
+        ConfigurationNode versionNode = config.getNode("version");
+        switch (versionNode.getInt()) {
+            case 1:
+                config.getNode("enabled").setComment("Enable/disable this plugin functionality").setValue(true);
+                config.getNode("maxBlocks").setComment("Maximum amount of blocks that will be destroyed in one hit").setValue(200);
+                break;
+            case 2:
+                config.getNode("item").setComment("The item that needs to be held.").setValue("minecraft:golden_axe");
+                config.getNode("consumeItem").setComment("Whether to consume the item after usage. NOT WORKING YET").setValue(false);
+                break;
+            case 3:
+                List<String> items = new ArrayList<>();
+                items.add(config.getNode("item").getString());
+                config.getNode("items").setComment("List of items that can be used as axe").setValue(items);
+                config.removeChild("item");
+                break;
+            case 4:
+                config.getNode("consumeDurability").setComment("Whether to consume durability from tools that support it.").setValue(true);
+                break;
+            case 5:
+                config.removeChild("consumeItem");
+                config.getNode("baseOnly").setComment("Only activate when the player hits the very bottom of the tree.").setValue(false);
+                config.getNode("placeSapling").setComment("Place the according sapling after logging the tree.").setValue(false);
+                config.getNode("saplingProtection").setComment("Amount of time in seconds to protect the placed sapling. (0 to disable)").setValue(60);
+                break;
+            default:
+                return false;
+        }
+        getLogger().info(String.format("Migrated from config version %d to %d", versionNode.getInt(), versionNode.getInt() + 1));
+        versionNode.setValue(versionNode.getInt() + 1);
+        return true;
+
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    /**
+     * Get the configuration. Only use after GameStartingServerEvent is over and config is initialized.
+     *
+     * @return config, if loaded
+     */
+    public CommentedConfigurationNode getConfig() {
+        if (config != null) {
+            return config;
+        } else {
+            getLogger().error("Config is not initialized!");
+            return null;
+        }
+    }
+
+    public SaplingProtectionHandler getSaplingHandler(){
+        return saplingHandler;
+    }
+
+
+    public ConfigurationLoader<CommentedConfigurationNode> getConfigManager() {
+        return configManager;
+    }
+}
---
+++ src/main/java/net/mineguild/minecraft/treedestroyage/event/BreakBlockHandler.java
@@ -0,0 +1,169 @@
+package net.mineguild.minecraft.treedestroyage.event;
+
+
+import com.flowpowered.math.vector.Vector3d;
+import com.google.common.collect.Lists;
+import com.google.common.reflect.TypeToken;
+import net.mineguild.minecraft.treedestroyage.TreeDestroyage;
+import net.mineguild.minecraft.treedestroyage.TreeDetector;
+import ninja.leaping.configurate.ConfigurationNode;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.block.BlockState;
+import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.data.Transaction;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.type.TreeType;
+import org.spongepowered.api.effect.sound.SoundTypes;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.gamemode.GameModes;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BreakBlockHandler {
+
+    private TreeDestroyage plugin;
+
+    private List<ChangeBlockEvent.Break> firedEvents = Lists.newArrayList();
+
+    public BreakBlockHandler(TreeDestroyage plugin) {
+        this.plugin = plugin;
+    }
+
+    @Listener
+    public void handle(ChangeBlockEvent.Break breakEvent) throws Exception {
+        if (breakEvent.getTransactions().size() > 1) {
+            return;
+        }
+        Transaction<BlockSnapshot> transaction = breakEvent.getTransactions().get(0);
+        boolean isBase = !TreeDetector.isWood(transaction.getOriginal().getLocation().get().sub(Vector3d.UP).createSnapshot());
+        if (getConfig().getNode("baseOnly").getBoolean()) {
+            if (!isBase) return;
+        }
+        if (!firedEvents.contains(breakEvent) && breakEvent.getCause().containsType(Player.class) && getConfig().getNode("enabled").getBoolean(true) && !breakEvent.isCancelled() &&
+                TreeDetector.isWood(transaction.getOriginal())) {
+            TreeType treeType = transaction.getOriginal().getState().get(Keys.TREE_TYPE).get();
+            Player cause = breakEvent.getCause().first(Player.class).get();
+            Optional<ItemStack> inHand = cause.getItemInHand();
+            List<String> items = getConfig().getNode("items").getList(TypeToken.of(String.class));
+            final boolean consumeDurability = getConfig().getNode("consumeDurability").getBoolean();
+            if (inHand.isPresent() && items.contains(inHand.get().getItem().getName()) && cause.hasPermission("TreeDestroyage.destroy")) {
+                ItemStack item = inHand.get();
+                int maxAmount = getConfig().getNode("maxBlocks").getInt();
+                if (consumeDurability && item.get(Keys.ITEM_DURABILITY).isPresent()) {
+                    int durability = item.get(Keys.ITEM_DURABILITY).get();
+                    maxAmount = durability + 2; // Because durability=0 is the last hit
+                    if (durability == 0) {
+                        plugin.getLogger().info("Cancelling here, durability is 0");
+                        return;
+                    }
+                }
+                TreeDetector dec = new TreeDetector(breakEvent.getTransactions().get(0).getOriginal(), maxAmount, getConfig());
+                Vector3d playerPos = cause.getLocation().getPosition();
+                List<Transaction<BlockSnapshot>> transactions = new ArrayList<>(dec.getWoodLocations().size());
+                dec.getWoodLocations().stream().sorted((snap1, snap2) -> {
+                    double distance1 = snap1.getPosition().toDouble().distance(playerPos);
+                    double distance2 = snap2.getPosition().toDouble().distance(playerPos);
+                    if (distance1 < distance2) {
+                        return -1;
+                    } else if (distance1 > distance2) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }).filter(snapshot -> !snapshot.equals(breakEvent.getTransactions().get(0).getOriginal())).forEach(blockSnapshot -> {
+                    BlockState newState = BlockTypes.AIR.getDefaultState();
+                    BlockSnapshot newSnapshot = blockSnapshot.withState(newState).withLocation(new Location<>(cause.getWorld(), blockSnapshot.getPosition()));
+                    Transaction<BlockSnapshot> t = new Transaction<>(blockSnapshot, newSnapshot);
+                    transactions.add(t);
+                });
+                transactions.forEach(blockSnapshotTransaction -> {
+                    ChangeBlockEvent.Break event = SpongeEventFactory.createChangeBlockEventBreak(Cause.builder().owner(cause).build(),
+                            cause.getWorld(), Lists.newArrayList(blockSnapshotTransaction));
+                    firedEvents.add(event);
+                    if (!getGame().getEventManager().post(event)) {
+                        if (cause.getGameModeData().get(Keys.GAME_MODE).get() != GameModes.CREATIVE) {
+                            BlockState state = blockSnapshotTransaction.getOriginal().getState();
+                            ItemStack.Builder builder = getGame().getRegistry().createBuilder(ItemStack.Builder.class);
+                            ItemStack itemStack = builder.itemType(state.getType().getDefaultState().getType().getItem().get()).build();
+                            itemStack.offer(Keys.TREE_TYPE, state.get(Keys.TREE_TYPE).get());
+                            Entity entity = cause.getWorld().createEntity(EntityTypes.ITEM, blockSnapshotTransaction.getOriginal().getPosition()).get(); // 'cause' is the player
+                            entity.offer(Keys.REPRESENTED_ITEM, itemStack.createSnapshot());
+                            cause.getWorld().spawnEntity(entity, Cause.builder().owner(cause).build());
+                            if (consumeDurability && item.supports(Keys.ITEM_DURABILITY)) {
+                                if (item.get(Keys.ITEM_DURABILITY).get() == 0) {
+                                    cause.getWorld().playSound(SoundTypes.ITEM_BREAK, cause.getLocation().getPosition(), 1);
+                                    cause.setItemInHand(null);
+                                } else {
+                                    item.offer(Keys.ITEM_DURABILITY, item.get(Keys.ITEM_DURABILITY).get() - 1);
+                                    cause.setItemInHand(item);
+                                }
+                            }
+                        }
+                        blockSnapshotTransaction.getFinal().restore(true, true);
+                    } else {
+                        // Event got canceled
+                        System.out.println("Event canceled.");
+                    }
+
+                });
+                firedEvents.clear();
+                if (isBase && getConfig().getNode("placeSapling").getBoolean()) {
+                    placeSapling(cause, breakEvent.getTransactions().get(0).getOriginal().getLocation().get(), treeType);
+                }
+            }
+        }
+    }
+
+    private void placeSapling(Player c, Location<World> treeBlock, TreeType treeType) {
+        Location baseBlock = treeBlock.sub(Vector3d.UP);
+        // Not yet implemented
+        /*Optional<ItemStackSnapshot> saplingSnapshot = ItemTypes.SAPLING.getTemplate().with(Keys.TREE_TYPE, treeType);
+        if(saplingSnapshot.isPresent()){
+            Optional<Set<BlockType>> placeableBlocks = saplingSnapshot.get().get(Keys.PLACEABLE_BLOCKS);
+            if (placeableBlocks.isPresent()){
+                if(placeableBlocks.get().contains(baseBlock.getBlockType())) {
+
+                }
+
+            } else {
+                System.out.println("Placeable blocks not present!");
+            }
+        }*/
+        if (baseBlock.getBlockType() == BlockTypes.DIRT || baseBlock.getBlockType() == BlockTypes.GRASS) {
+            BlockSnapshot old = treeBlock.getBlock().snapshotFor(treeBlock);
+            BlockSnapshot newBL = old.withState(BlockState.builder().blockType(BlockTypes.SAPLING).build().with(Keys.TREE_TYPE, treeType).get());
+            Transaction<BlockSnapshot> transaction = new Transaction<>(old, newBL);
+            List<Transaction<BlockSnapshot>> transactions = Lists.newArrayList();
+            transactions.add(transaction);
+            ChangeBlockEvent.Place event = SpongeEventFactory.createChangeBlockEventPlace(Cause.builder().owner(c).build(), c.getWorld(), transactions);
+            if (!Sponge.getEventManager().post(event)) {
+                transaction.getFinal().restore(true, true);
+                if (getConfig().getNode("saplingProtection").getInt() > 0) {
+                    plugin.getSaplingHandler().addProtectedSapling(treeBlock);
+                }
+            }
+        }
+
+    }
+
+    private ConfigurationNode getConfig() {
+        return plugin.getConfig();
+    }
+
+    private Game getGame() {
+        return plugin.getGame();
+    }
+}
