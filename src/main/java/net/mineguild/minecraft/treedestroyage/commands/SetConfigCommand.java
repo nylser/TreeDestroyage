6da580fc42b04206513fb8dae53cdd1df225b1c9
Korbinian Stein <korbinian.stein@googlemail.com>
2016-06-01 18:36:32 +0200

    Add explicit API 4.1.0-SNAPSHOT version

--- src/main/java/net/mineguild/minecraft/treedestroyage/commands/SetConfigCommand.java
+++ src/main/java/net/mineguild/minecraft/treedestroyage/commands/SetConfigCommand.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-@SuppressWarnings("NullableProblems")
 public class SetConfigCommand implements CommandExecutor {
     private TreeDestroyage plugin;
 
